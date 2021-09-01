480d043177c58e2ab9913e2c7d32b899c3b236fd
Sam Myers <sam@themyers.party>
2021-09-01 08:28:06 -0700

    testing v0.0.3

--- Hootsi-Helpr.user.js
+++ Hootsi-Helpr.user.js
@@ -3,7 +3,7 @@
 // @downloadURL  https://github.com/samstevenm/hootsi-helpr/raw/main/Hootsi-Helpr.user.js
 // @updateURL    https://github.com/samstevenm/hootsi-helpr/raw/main/Hootsi-Helpr.user.js
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Improve Hootsi functionality!
 // @author       Sam Myers
 // @match        https://www.hootsi.com/*
@@ -17,7 +17,7 @@ jQuery(function($){
     $(document).ready(function($) {
         var _highest = 0;
 
-        //localStorage.clear();
+        localStorage.clear();
 
         //figure out which div is highest so widget stays on top
         $("div").each(function() {
@@ -137,48 +137,7 @@ jQuery(function($){
             e.preventDefault();
         });
 
-    // Sam changes for Al 2021-08-10
-    // Strips non-hex characters and makes MAC field uppercase
-    $("#sonos").click(function(e) {
-            $("[name='mac']").prop("id","mac");
-            $("[name='serial']").prop("id","serial");
 
-
-
-        $('#serial').blur(function(e) {
-           // if ("#sonos".is(':checked')) {
-            //alert("BLUR");
-            var orig_serial = $('#serial').val();
-            //alert( "Untrimmed: " + orig_serial );
-            //remove all non-hex characters and make UPPER
-            var cleaned_mac = orig_serial.replace(/[^a-fA-F0-9\s]/gi, '').toUpperCase();
-            //shorten to 12 characters long
-            var trim_clean_mac = cleaned_mac.substring(0,12);
-            //alert( "Trim & Clean: " + trim_clean_mac);
-            //update the field to the new cleaned and trimmed MAC
-            if ( $("#sonos").is(':checked') ) {
-            $('#mac').val(trim_clean_mac);
-            }
-            //e.preventDefault();
-           // }
-        });
-
-
-        $('#mac').blur(function(e) {
-            var orig_mac = $('#mac').val();
-            //alert( "Original MAC:" + orig_mac);
-            //remove all non-hex characters and make UPPER
-            var cleaned_mac = orig_mac.replace(/[^a-fA-F0-9\s]/gi, '').toUpperCase();
-            //shorten to 12 characters long
-            var trim_clean_mac = cleaned_mac.substring(0,12);
-            //alert( "Trim & Clean: " + trim_clean_mac);
-            //update the field to the new cleaned and trimmed MAC
-            $('#mac').val(trim_clean_mac);
-            e.preventDefault();
-            });
-
-        //e.preventDefault();
-            });
 
     });
 });
